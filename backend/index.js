c3dbfbc893604324be5a8ca1d08de6ba4256d0ca
truongquoc <quoc93346@gmail.com>
2026-04-19 20:27:04 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -57,3 +57,14 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
+/**
+npx sequelize-cli model:generate --name GIOHANG --attributes "" --force
+
+run migrations: 
+npx sequelize-cli db:migrate
+
+npx sequelize-cli db:migrate:undo
+
+SELECT * FROM information_schema.table_constraints
+    -> WHERE table_schema = 'shop_laptop' AND table_name ='giohangs';
+ */
