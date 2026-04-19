c26aaf45c332d05ebbd91164664e771b3c7ec464
truongquoc <quoc93346@gmail.com>
2026-04-19 15:55:35 +0700

    Create 20260419081939-create-thongke.js

---
+++ backend/migrations/20260419081939-create-thongke.js
@@ -0,0 +1,61 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('thongkes', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.STRING
+      },
+      ThangNam: {
+        type: Sequelize.STRING
+      },
+      MaSP: {
+        type: Sequelize.STRING,
+        references: {
+          model: 'sanphams',
+          key: 'id',
+        },
+      },
+      TenSP: {
+        type: Sequelize.STRING
+      },
+      SoLuongBan: {
+        type: Sequelize.INTEGER
+      },
+      GiaBan: {
+        type: Sequelize.INTEGER
+      },
+      TongTienSP: {
+        type: Sequelize.INTEGER
+      },
+      DoanhThuThang: {
+        type: Sequelize.INTEGER
+      },
+      ChiPhiThang: {
+        type: Sequelize.INTEGER
+      },
+      LoiNhuanThang: {
+        type: Sequelize.INTEGER
+      },
+      NgayThongKe: {
+        type: Sequelize.DATE
+      },
+      GhiChu: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('thongkes');
+  }
+};
