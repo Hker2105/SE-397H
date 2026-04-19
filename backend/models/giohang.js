6f8d36f0a581d06c75d9cd70f160fd98309d656d
truongquoc <quoc93346@gmail.com>
2026-04-19 15:56:21 +0700

    Create giohang.js

---
+++ backend/models/giohang.js
@@ -0,0 +1,32 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class GIOHANG extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      GIOHANG.belongsTo(models.KHACHHANG,{
+        foreignKey: 'MaKhachHang'
+      })
+      GIOHANG.belongsTo(models.SANPHAM,{
+        foreignKey: 'MaSP'
+      })
+    }
+  }
+  GIOHANG.init({
+    MaKhachHang: DataTypes.STRING,
+    MaSP: DataTypes.STRING,
+    NgayTao: DataTypes.DATE,
+    SoLuong: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'GIOHANG',
+  });
+  return GIOHANG;
+};
