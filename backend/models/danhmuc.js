86f7c1585e8b8c59bc4c8d73b59681f9fb6fb0db
truongquoc <quoc93346@gmail.com>
2026-04-19 15:56:18 +0700

    Create danhmuc.js

---
+++ backend/models/danhmuc.js
@@ -0,0 +1,29 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class DANHMUC extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      DANHMUC.hasMany(models.SANPHAM, {
+          foreignKey: 'MaDM',
+      });
+    }
+  }
+  DANHMUC.init({
+    TenDanhMuc: DataTypes.STRING,
+    MoTa: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'DANHMUC',
+    tableName: 'danhmucs',
+    underscored: true
+  });
+  return DANHMUC;
+};
