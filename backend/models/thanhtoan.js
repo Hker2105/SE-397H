3e5641e26d14831c57c1a7f8511e5d3bea781e1a
truongquoc <quoc93346@gmail.com>
2026-04-19 15:56:52 +0700

    Create thanhtoan.js

---
+++ backend/models/thanhtoan.js
@@ -0,0 +1,29 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class THANHTOAN extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      THANHTOAN.belongsTo(models.DONHANG,{
+        foreignKey: 'MaDH'
+      })
+    }
+  }
+  THANHTOAN.init({
+    MaDH: DataTypes.STRING,
+    HinhThuc: DataTypes.STRING,
+    NgayThanhToan: DataTypes.DATE,
+    TrangThai: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'THANHTOAN',
+  });
+  return THANHTOAN;
+};
