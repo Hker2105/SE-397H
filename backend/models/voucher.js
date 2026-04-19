f3102a575fcbdf1dcbbce68ed6f5ae22a86251ea
truongquoc <quoc93346@gmail.com>
2026-04-19 15:56:58 +0700

    Create voucher.js

---
+++ backend/models/voucher.js
@@ -0,0 +1,30 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class VOUCHER extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      VOUCHER.hasMany(models.GIOHANG, {
+          foreignKey: 'MaVC',
+      });
+    }
+  }
+  VOUCHER.init({
+    MaVoucher: DataTypes.STRING,
+    GiaTri: DataTypes.INTEGER,
+    NgayBD: DataTypes.DATE,
+    NgayKT: DataTypes.DATE,
+    SoLuong: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'VOUCHER',
+  });
+  return VOUCHER;
+};
