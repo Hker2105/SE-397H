054a956327b6b49966034ef1178c636b6bef066a
truongquoc <quoc93346@gmail.com>
2026-04-19 15:56:20 +0700

    Create donhang.js

---
+++ backend/models/donhang.js
@@ -0,0 +1,36 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class DONHANG extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      DONHANG.belongsTo(models.KHACHHANG,{
+        foreignKey: 'MaKhachHang'
+      })
+      DONHANG.belongsTo(models.VOUCHER,{
+        foreignKey: 'MaVC'
+      })
+    }
+  }
+  DONHANG.init({
+    MaKhachHang: DataTypes.STRING,
+    MaVC: DataTypes.STRING,
+    SoLuong: DataTypes.INTEGER,
+    DonGia: DataTypes.INTEGER,
+    NgayDat: DataTypes.DATE,
+    TongTien: DataTypes.INTEGER,
+    TinhTrang: DataTypes.STRING,
+    GhiChu: DataTypes.TEXT
+  }, {
+    sequelize,
+    modelName: 'DONHANG',
+  });
+  return DONHANG;
+};
