0c5c7261a8cda107322ed48ba338190233f0f7fd
truongquoc <quoc93346@gmail.com>
2026-04-19 15:56:41 +0700

    Create nhacungcap.js

---
+++ backend/models/nhacungcap.js
@@ -0,0 +1,29 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class NHACUNGCAP extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      NHACUNGCAP.hasMany(models.SANPHAM, {
+          foreignKey: 'MaNCC',
+      });
+    }
+  }
+  NHACUNGCAP.init({
+    TenNCC: DataTypes.STRING,
+    SoDienThoai: DataTypes.STRING,
+    DiaChi: DataTypes.STRING,
+    Email: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'NHACUNGCAP',
+  });
+  return NHACUNGCAP;
+};
