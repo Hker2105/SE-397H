95a592383a5041aa6408d8a71b238f46238e1ad7
truongquoc <quoc93346@gmail.com>
2026-04-19 15:56:35 +0700

    Create khachhang.js

---
+++ backend/models/khachhang.js
@@ -0,0 +1,35 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class KHACHHANG extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      KHACHHANG.hasMany(models.GIOHANG, {
+          foreignKey: 'MaKhachHang',
+      });
+      KHACHHANG.hasMany(models.DONHANG, {
+          foreignKey: 'MaKhachHang',
+      });
+    }
+  }
+  KHACHHANG.init({
+    HoTen: DataTypes.STRING,
+    Email: DataTypes.STRING,
+    MatKhau: DataTypes.STRING,
+    SoDienThoai: DataTypes.STRING,
+    DiaChi: DataTypes.STRING,
+    LoaiTaiKhoan: DataTypes.STRING,
+    NgayTao: DataTypes.DATE
+  }, {
+    sequelize,
+    modelName: 'KHACHHANG',
+  });
+  return KHACHHANG;
+};
