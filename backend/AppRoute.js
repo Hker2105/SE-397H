a60bb3193f151a134885c0b42008c3e9d4f55ebf
truongquoc <quoc93346@gmail.com>
2026-04-19 22:20:06 +0700

    Create AppRoute.js

---
+++ backend/AppRoute.js
@@ -0,0 +1,12 @@
+import express from 'express'
+import * as sanPhamController from './controllers/sanPhamController'
+const router = express.Router()
+export function AppRoute(app) {
+    //http:localhost:3000/sanphams
+    router.get('/sanphams', sanPhamController.getsanPham)
+    router.get('/sanphams/:id', sanPhamController.getsanPhamById)
+    router.post('/sanphams', sanPhamController.insertsanPham)
+    router.put('/sanphams', sanPhamController.updatesanPham)
+    router.delete('/sanphams/:id', sanPhamController.deletesanPham)
+    app.use('/api/', router)
+}
