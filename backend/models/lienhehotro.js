4f5f59e2dd3cc17a67026395f2f5743c63e9830a
truongquoc <quoc93346@gmail.com>
2026-04-19 15:56:39 +0700

    Create lienhehotro.js

---
+++ backend/models/lienhehotro.js
@@ -0,0 +1,30 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class LIENHEHOTRO extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      LIENHEHOTRO.belongsTo(models.KHACHHANG,{
+        foreignKey: 'MaKhachHang'
+      })
+    }
+  }
+  LIENHEHOTRO.init({
+    MaKhachHang: DataTypes.STRING,
+    TieuDe: DataTypes.STRING,
+    Noidung: DataTypes.TEXT,
+    NgayGui: DataTypes.DATE,
+    TrangThai: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'LIENHEHOTRO',
+  });
+  return LIENHEHOTRO;
+};
