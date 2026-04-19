5b2a34a3f77597057f466742e473c1dc04f3b69e
truongquoc <quoc93346@gmail.com>
2026-04-19 15:55:09 +0700

    Create 20260418155747-create-khachhang.js

---
+++ backend/migrations/20260418155747-create-khachhang.js
@@ -0,0 +1,46 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('khachhangs', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.STRING
+      },
+      HoTen: {
+        type: Sequelize.STRING
+      },
+      Email: {
+        type: Sequelize.STRING,
+        unique: true 
+      },
+      MatKhau: {
+        type: Sequelize.STRING
+      },
+      SoDienThoai: {
+        type: Sequelize.STRING
+      },
+      DiaChi: {
+        type: Sequelize.STRING
+      },
+      LoaiTaiKhoan: {
+        type: Sequelize.STRING
+      },
+      NgayTao: {
+        type: Sequelize.DATE
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('khachhangs');
+  }
+};
