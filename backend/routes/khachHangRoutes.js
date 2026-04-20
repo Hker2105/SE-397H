23c6c5a1a24eeedea5da06189033fe11ca457ef2
truongquoc <quoc93346@gmail.com>
2026-04-20 08:52:11 +0700

    Update khachHangRoutes.js

--- backend/routes/khachHangRoutes.js
+++ backend/routes/khachHangRoutes.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const khachHangController = require('../controllers/khachHangController');
-const authMiddleware = require('../middleware/authMiddleware');
+// const express = require('express');
+// const khachHangController = require('../controllers/khachHangController');
+// const authMiddleware = require('../middleware/authMiddleware');
 
-const router = express.Router();
+// const router = express.Router();
 
-router.get('/profile', authMiddleware, khachHangController.getProfile);
-router.put('/profile', authMiddleware, khachHangController.updateProfile);
-router.get('/', authMiddleware, khachHangController.getAllUsers);
-router.delete('/me', authMiddleware, khachHangController.deleteAccount);
+// router.get('/profile', authMiddleware, khachHangController.getProfile);
+// router.put('/profile', authMiddleware, khachHangController.updateProfile);
+// router.get('/', authMiddleware, khachHangController.getAllUsers);
+// router.delete('/me', authMiddleware, khachHangController.deleteAccount);
 
-module.exports = router;
+// module.exports = router;
