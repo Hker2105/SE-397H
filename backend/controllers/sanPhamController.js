996b38bc1ba1aafe88a1d45d794e65308c985715
truongquoc <quoc93346@gmail.com>
2026-04-21 20:26:12 +0700

    Update sanPhamController.js

--- backend/controllers/sanPhamController.js
+++ backend/controllers/sanPhamController.js
@@ -194,34 +194,73 @@
 //         });
 //     }
 // };
-import { Sequelize } from "sequelize"
+import { Sequelize, where } from "sequelize"
+const {Op} = Sequelize;
 import db from "../models"
+import insertsanPhamRequest from "../dtos/requests/insertsanPhamRequests"
+
 export async function getsanPham(req, res){
+    //const sanPhams = await db.SANPHAM.findAll()
+    const {search ='', page = 1} = req.query;
+    const pageSize = 10;
+    const offset = (page - 1) * pageSize;
+    let whereClause = {};
+    if (search.trim() !== '') {
+        whereClause = {
+            [Op.or]: [
+                {TenSP: {[Op.like]: `%${search}%`}},
+                {MoTa: {[Op.like]: `%${search}%`}}
+            ]              
+        }
+    }
+    const [sanPhams, totalsanPhams] = await Promise.all([
+        db.SANPHAM.findAll({
+            where: whereClause,
+            limit: pageSize,
+            offset: offset
+        }),
+        db.SANPHAM.count({
+            where: whereClause
+        })
+    ]);
     res.status(200).json({
-        message: 'Lấy danh sách sản phẩm thành công'
-    })
+        message: 'Lấy danh sách sản phẩm thành công',
+        data: sanPhams,
+        currentPage: parseInt(page, 10),
+        totalPages: Math.ceil(totalsanPhams / pageSize),
+        totalsanPhams
+    });
 }
 
 export async function getsanPhamById(req, res){
+    const {id} = req.params
+    const sanPham = await db.SANPHAM.findByPk(id)
+
+    if(!sanPham) {
+        return res.status(400).json({
+            message: 'Không tìm thấy sản phẩm'
+        });
+    }
     res.status(200).json({
-        message: 'Lấy thông tin sản phẩm thành công'
+        message: 'Lấy thông tin sản phẩm thành công',
+        data: sanPham
     })
 }
 
 export async function insertsanPham(req, res){
-    try {
-        // console.log(JSON.stringify(req.body))
-        const sanPham = await db.SANPHAM.create(req.body)
-        res.status(201).json({
-            message: 'Thêm mới sản phẩm thành công',
-            data: sanPham
-        })
-    } catch (error) {
-        res.status(500).json({
-            message: 'Xảy ra lỗi khi thêm sản phẩm',
-            error: error.message
-        })
-    }
+    // const {error} = insertsanPhamRequest.validate(req.body)
+    // if(error) {
+    //     res.status(400).json({
+    //         message: 'Xảy ra lỗi khi thêm sản phẩm',
+    //         error: error.details[0]?.message 
+    //     });
+    // }
+    // console.log(JSON.stringify(req.body))
+    const sanPham = await db.SANPHAM.create(req.body)
+    return res.status(201).json({
+        message: 'Thêm mới sản phẩm thành công',
+        data: sanPham
+    })
 }
 
 export async function deletesanPham(req, res){
