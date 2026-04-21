e02671563f81e74df9da270df896c19432569997
truongquoc <quoc93346@gmail.com>
2026-04-21 20:26:09 +0700

    Update AppRoute.js

--- backend/AppRoute.js
+++ backend/AppRoute.js
@@ -5,44 +5,49 @@ import * as nhaCungCapController from './controllers/nhaCungCapController'
 import * as donHangController from './controllers/donHangController'
 import * as voucherController from './controllers/voucherController'
 import * as danhMucController from './controllers/danhMucController'
+import asyncHandler from './middlewares/asyncHandler'
+import validate from './middlewares/validate'
+import insertsanPhamRequest from './dtos/requests/insertsanPhamRequests'
 const router = express.Router()
+
+
 export function AppRoute(app) {
     //http:localhost:3000/sanphams
-    router.get('/sanphams', sanPhamController.getsanPham)
-    router.get('/sanphams/:id', sanPhamController.getsanPhamById)
-    router.post('/sanphams', sanPhamController.insertsanPham)
-    router.put('/sanphams', sanPhamController.updatesanPham)
-    router.delete('/sanphams/:id', sanPhamController.deletesanPham)
+    router.get('/sanphams', asyncHandler(sanPhamController.getsanPham))
+    router.get('/sanphams/:id', asyncHandler(sanPhamController.getsanPhamById))
+    router.post('/sanphams', validate(insertsanPhamRequest),asyncHandler(sanPhamController.insertsanPham))
+    router.put('/sanphams', asyncHandler(sanPhamController.updatesanPham))
+    router.delete('/sanphams/:id', asyncHandler(sanPhamController.deletesanPham))
 
-    router.get('/hangsanxuats', hangSanXuatController.getHangSanXuats)
-    router.get('/hangsanxuats/:id', hangSanXuatController.getHangSanXuatById)
-    router.post('/hangsanxuats', hangSanXuatController.insertHangSanXuat)
-    router.put('/hangsanxuats/:id', hangSanXuatController.updateHangSanXuat)
-    router.delete('/hangsanxuats/:id', hangSanXuatController.deleteHangSanXuat)
+    router.get('/hangsanxuats', asyncHandler(hangSanXuatController.getHangSanXuats))
+    router.get('/hangsanxuats/:id', asyncHandler(hangSanXuatController.getHangSanXuatById))
+    router.post('/hangsanxuats', asyncHandler(hangSanXuatController.insertHangSanXuat))
+    router.put('/hangsanxuats/:id', asyncHandler(hangSanXuatController.updateHangSanXuat))
+    router.delete('/hangsanxuats/:id', asyncHandler(hangSanXuatController.deleteHangSanXuat))
 
-    router.get('/nhacungcaps', nhaCungCapController.getNhaCungCaps)
-    router.get('/nhacungcaps/:id', nhaCungCapController.getNhaCungCapById)
-    router.post('/nhacungcaps', nhaCungCapController.insertNhaCungCap)
-    router.put('/nhacungcaps/:id', nhaCungCapController.updateNhaCungCap)
-    router.delete('/nhacungcaps/:id', nhaCungCapController.deleteNhaCungCap)
+    router.get('/nhacungcaps', asyncHandler(nhaCungCapController.getNhaCungCaps))
+    router.get('/nhacungcaps/:id', asyncHandler(nhaCungCapController.getNhaCungCapById))
+    router.post('/nhacungcaps', asyncHandler(nhaCungCapController.insertNhaCungCap))
+    router.put('/nhacungcaps/:id', asyncHandler(nhaCungCapController.updateNhaCungCap))
+    router.delete('/nhacungcaps/:id', asyncHandler(nhaCungCapController.deleteNhaCungCap))
 
-    router.get('/donhangs', donHangController.getDonHangs)
-    router.get('/donhangs/:id', donHangController.getDonHangById)
-    router.post('/donhangs', donHangController.insertDonHang)
-    router.put('/donhangs/:id', donHangController.updateDonHang)
-    router.delete('/donhangs/:id', donHangController.deleteDonHang)
+    router.get('/donhangs', asyncHandler(donHangController.getDonHangs))
+    router.get('/donhangs/:id', asyncHandler(donHangController.getDonHangById))
+    router.post('/donhangs', asyncHandler(donHangController.insertDonHang))
+    router.put('/donhangs/:id', asyncHandler(donHangController.updateDonHang))
+    router.delete('/donhangs/:id', asyncHandler(donHangController.deleteDonHang))
 
-    router.get('/vouchers', voucherController.getVouchers)
-    router.get('/vouchers/:id', voucherController.getVoucherById)
-    router.post('/vouchers', voucherController.insertVoucher)
-    router.put('/vouchers/:id', voucherController.updateVoucher)
-    router.delete('/vouchers/:id', voucherController.deleteVoucher)
+    router.get('/vouchers', asyncHandler(voucherController.getVouchers))
+    router.get('/vouchers/:id', asyncHandler(voucherController.getVoucherById))
+    router.post('/vouchers', asyncHandler(voucherController.insertVoucher))
+    router.put('/vouchers/:id', asyncHandler(voucherController.updateVoucher))
+    router.delete('/vouchers/:id', asyncHandler(voucherController.deleteVoucher))
 
-    router.get('/danhmucs', danhMucController.getDanhMucs)
-    router.get('/danhmucs/:id', danhMucController.getDanhMucById)
-    router.post('/danhmucs', danhMucController.insertDanhMuc)
-    router.put('/danhmucs/:id', danhMucController.updateDanhMuc)
-    router.delete('/danhmucs/:id', danhMucController.deleteDanhMuc)
+    router.get('/danhmucs', asyncHandler(danhMucController.getDanhMucs))
+    router.get('/danhmucs/:id', asyncHandler(danhMucController.getDanhMucById))
+    router.post('/danhmucs', asyncHandler(danhMucController.insertDanhMuc))
+    router.put('/danhmucs/:id', asyncHandler(danhMucController.updateDanhMuc))
+    router.delete('/danhmucs/:id', asyncHandler(danhMucController.deleteDanhMuc))
 
     app.use('/api/', router)
 }
