703c85c8159f61a812956e84410a61ac89953e8e
truongquoc <quoc93346@gmail.com>
2026-04-22 20:45:17 +0700

    Update hangSanXuatController.js

--- backend/controllers/hangSanXuatController.js
+++ backend/controllers/hangSanXuatController.js
@@ -1,31 +1,36 @@
 import { Sequelize } from "../models"
 import db from "../models"
 export async function getHangSanXuats(req, res){
+    const hangSanXuats = await db.HANGSANXUAT.findAll();
     res.status(200).json({
-        message: 'Lấy danh sách hãng sản xuất thành công'
-    })
+        message: 'Lấy danh sách hãng sản xuất thành công',
+        data: hangSanXuats
+    });
 }
 
 export async function getHangSanXuatById(req, res){
+    const {id} = req.params.id;
+    const hangSanXuats = await db.HANGSANXUAT.findById(id);
+
+    if(!hangSanXuats) {
+        return res.status(404).json({
+            message: 'Không tìm thấy nhà cung cấp này'
+        })
+    }
+
     res.status(200).json({
-        message: 'Lấy thông tin hãng sản xuất thành công'
-    })
+        message: 'Lấy thông tin hãng sản xuất thành công',
+        data: hangSanXuats
+    });
 }
 
 export async function insertHangSanXuat(req, res){
-    try {
         // console.log(JSON.stringify(req.body))
         const hangSanXuat = await db.HANGSANXUAT.create(req.body)
         res.status(201).json({
             message: 'Thêm mới nhà cung cấp thành công',
             data: hangSanXuat
         })
-    } catch (error) {
-        res.status(500).json({
-        message: 'Xảy ra lỗi khi thêm nhà cung cấp',
-        error: error.message
-        })
-    }
 }
 
 export async function deleteHangSanXuat(req, res){
