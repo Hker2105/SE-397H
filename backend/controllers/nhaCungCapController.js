1e072ea7f635139c680cff566da57042f644f669
truongquoc <quoc93346@gmail.com>
2026-04-22 20:45:18 +0700

    Update nhaCungCapController.js

--- backend/controllers/nhaCungCapController.js
+++ backend/controllers/nhaCungCapController.js
@@ -1,31 +1,36 @@
 import { Sequelize } from "../models"
 import db from "../models"
 export async function getNhaCungCaps(req, res){
+    const nhaCungCaps = await db.NHACUNGCAP.findAll();
     res.status(200).json({
-        message: 'Lấy danh sách nhà cung cấp thành công'
-    })
+        message: 'Lấy danh sách nhà cung cấp thành công',
+        data: nhaCungCaps
+    });
 }
 
 export async function getNhaCungCapById(req, res){
+    const {id} = req.params.id;
+    const nhaCungCaps = await db.NHACUNGCAP.findByPk(id);
+    
+    if(!nhaCungCaps) {
+        return res.status(404).json({
+            message: 'Không tìm thấy nhà cung cấp này'
+        })
+    }
     res.status(200).json({
-        message: 'Lấy thông tin nhà cung cấp thành công'
-    })
+        message: 'Lấy thông tin nhà cung cấp thành công',
+        data: nhaCungCaps
+    });
 }
 
 export async function insertNhaCungCap(req, res){
-    try {
+   
         //console.log(JSON.stringify(req.body))
         const nhaCungCap = await db.NHACUNGCAP.create(req.body)
         res.status(201).json({
             message: 'Thêm mới nhà cung cấp thành công',
             data: nhaCungCap
         })
-    } catch (error) {
-            res.status(500).json({
-            message: 'Xảy ra lỗi khi thêm nhà cung cấp',
-            error: error.message
-        })
-    }
 }
 
 export async function deleteNhaCungCap(req, res){
