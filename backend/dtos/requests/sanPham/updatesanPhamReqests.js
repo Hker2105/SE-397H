3eff8c0891f36cc8dbcc565859cee56c2b2d67fe
truongquoc <quoc93346@gmail.com>
2026-04-25 21:13:59 +0700

    Create updatesanPhamReqests.js

---
+++ backend/dtos/requests/sanPham/updatesanPhamReqests.js
@@ -0,0 +1,39 @@
+import Joi from 'joi'
+
+const updateSanPhamSchema = Joi.object({
+    MaSP: Joi.string().optional(),
+    TenSP: Joi.string().optional(),
+    MaDM: Joi.string().optional(),
+    MaHang: Joi.string().optional(),
+    MaNCC: Joi.string().optional(),
+    MoTa: Joi.string().optional(),
+    Gia: Joi.number().positive().optional(),
+    SoLuongTon: Joi.number().integer().min(0).optional(),
+    HinhAnh: Joi.string().allow("").optional(),
+    UuDaiSV: Joi.boolean().optional(),
+    NgayThem: Joi.date().optional(),
+    createdAt: Joi.date().optional(), 
+    updatedAt: Joi.date().optional()  
+})
+
+class updatesanPhamRequest{
+    constructor(data) {
+        this.MaSP = data.MaSP
+        this.TenSP = data.TenSP
+        this.MaDM = data.MaDM
+        this.MaHang = data.MaHang
+        this.MaNCC = data.MaNCC
+        this.MoTa = data.MoTa
+        this.Gia = data.Gia
+        this.SoLuongTon = data.SoLuongTon
+        this.HinhAnh = data.HinhAnh
+        this.UuDaiSV = data.UuDaiSV
+        this.NgayThem = data.NgayThem
+    }
+
+    static validate(data){
+        return updateSanPhamSchema.validate(data)
+    }
+}
+
+export default updatesanPhamRequest;
