3328677f96d37143dacf8a1f3dc19643cd72efaf
truongquoc <quoc93346@gmail.com>
2026-04-25 21:13:44 +0700

    Update donHangController.js

--- backend/controllers/donHangController.js
+++ backend/controllers/donHangController.js
@@ -1,3 +1,7 @@
+import { Sequelize, where } from "sequelize";
+const {Op} = Sequelize;
+import db from "../models";
+import insertDonHangRequest from '../dtos/requests/donHang/insertDonHangRequests';
 export async function getDonHangs(req, res){
     try {
         const donHangs = await db.DONHANG.findAll();
@@ -54,26 +58,35 @@ export async function insertDonHang(req, res){
 }
 
 export async function deleteDonHang(req, res){
+       try {
         const { id } = req.params;
-        const deleted = await db.DONHANG.destroy({ where: { id } });
+        const deleted = await db.DONHANG.destroy({ where: { MaDH: id } });
         if(deleted) {
-           return res.status(200).json({ message: 'Xoá đơn hàng thành công' })
+            return res.status(200).json({ message: 'Xoá đơn hàng thành công' })
         } else {
-            return res.status(404).json({
-                message: 'Đơn hàng không tìm thấy'
-            })
+            return res.status(404).json({ message: 'Đơn hàng không tìm thấy' })
         }
+    } catch (error) {
+        res.status(500).json({ message: 'Xảy ra lỗi', error: error.message })
+    }
 }
 
 export async function updateDonHang(req, res){
     try {
-        const { id } = req.params;
-        const { SoLuong, DonGia, TongTien, TinhTrang, GhiChu } = req.body
-        await db.DONHANG.update(
-            { SoLuong, DonGia, TongTien, TinhTrang, GhiChu },
-            { where: { MaDH: id } }
-        )
-        res.status(200).json({ message: 'Update đơn hàng thành công' })
+        const { error } = insertDonHangRequest.validate(req.body)
+        if(error) {
+            return res.status(400).json({
+                message: 'Dữ liệu không hợp lệ',
+                error: error.details[0].message
+            })
+        }
+        const id = req.params.id
+        const updated = await db.DONHANG.update(req.body, { where: { MaDH: id } });
+        if(updated[0] > 0) {
+            return res.status(200).json({ message: 'Update đơn hàng thành công' })
+        } else {
+            return res.status(404).json({ message: 'Đơn hàng không tìm thấy' })
+        }
     } catch (error) {
         res.status(500).json({ message: 'Xảy ra lỗi', error: error.message })
     }
