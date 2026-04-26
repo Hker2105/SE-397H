81a7a6f7330ad207881f8a99ad35a82416c183a5
truongquoc <quoc93346@gmail.com>
2026-04-26 22:21:20 +0700

    Create khachHangResponese.js

---
+++ backend/dtos/responese/khachHang/khachHangResponese.js
@@ -0,0 +1,12 @@
+class khachHangResponses {
+    constructor(khachHang) {
+        this.MaKhachHang = khachHang.MaKhachHang;
+        this.HoTen = khachHang.HoTen;
+        this.Email = khachHang.Email;
+        this.SoDienThoai = khachHang.SoDienThoai;
+        this.DiaChi = khachHang.DiaChi;
+        this.LoaiTaiKhoan = khachHang.LoaiTaiKhoan;
+        this.NgayTao = khachHang.NgayTao;
+    }
+}
+export default khachHangResponses
