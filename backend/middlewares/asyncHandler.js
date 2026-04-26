dec7018ff7dfd0a8865c7648be9772f6c8b15810
truongquoc <quoc93346@gmail.com>
2026-04-26 22:21:25 +0700

    Update asyncHandler.js

--- backend/middlewares/asyncHandler.js
+++ backend/middlewares/asyncHandler.js
@@ -5,6 +5,8 @@ const asyncHandler = (fn) => {
         try {
             await fn(req, res, next)
         }catch(error) {
+            console.error('Detailed Error: ', error);
+            console.log('Error Details:', {message:error.message, stack:error.stack});
             return res.status(500).json({
                 message: 'Internal Sever Error',
                 error: process.env.NODE_ENV === 'development' ? error : ''
